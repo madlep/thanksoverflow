54b85473c52e5bef6835ae2e0c7c5fc65fe0b432
Julian Doherty (madlep) <madlep@madlep.com>
2020-02-25 23:10:39 +1100

    v04 and_then

---
+++ lib/v04/admin_controller.rb
@@ -0,0 +1,30 @@
+class V04::AdminController < ActionController::Base
+  THANKS_ID = 31
+
+  def index
+    render 'admin/index'
+  end
+
+  def import_credits
+    importer = V04::CreditImporter.new(
+      person_id: THANKS_ID,
+      api_key: Rails.configuration.tmdb_api_key
+    )
+
+    case importer.import()
+    in V04::Core::Success[
+      V04::Core::ImportSummary[inserted_count, updated_count, error_count]
+    ]
+      flash[:notice] = "Imported #{inserted_count} new credits, updated #{updated_count}"
+      flash[:error] = "#{error_count} titles had errors preventing saving" if error_count > 0
+    in V04::Core::Failure[message]
+      flash[:error] = "Fatal error prevented import: #{message.inspect}"
+    end
+  end
+
+  def reset_credits
+    deleted_count = Title.delete_all
+    flash[:notice] = "Deleted #{deleted_count} titles"
+    redirect_to action: :index
+  end
+end 
---
+++ lib/v04/credit_importer.rb
@@ -0,0 +1,65 @@
+class V04::CreditImporter
+  include V04::Core
+
+  def initialize(person_id:, api_key:)
+    @person_id = person_id
+    @api_key = api_key
+  end
+
+  def import()
+    fetch_credits()
+      .and_then(&method(:parse_credits))
+      .and_then(){|credits|
+        credits["cast"]
+        .reduce(ImportSummary.new(0,0,0), &method(:create_or_update_title))
+        .then(){|final_summary|
+          Success.new(final_summary)
+        }
+      }
+  end
+
+  private
+  def fetch_credits()
+    url = "https://api.themoviedb.org/3/person/#{@person_id}/combined_credits?api_key=#{@api_key}&language=en-US"
+    response = HTTP.get(url)
+    if response.status.success?
+      Success.new(response.to_s)
+    else
+      Failure.new("error fetching credits. Status=#{response.status}")
+    end
+  rescue HTTP::Error => e
+    Failure.new(e)
+  end
+
+  def parse_credits(credits_json)
+    begin
+      Success.new(JSON.parse(credits_json))
+    rescue JSON::ParserError => e
+      Failure.new(e)
+    end
+  end
+
+  def create_or_update_title(import_summary, cast_entry)
+    title = Title.where(tmdb_id: cast_entry["id"]).first
+    if title
+      title.update( popularity: cast_entry["popularity"], synced_at: DateTime.now)
+      import_summary.updated()
+    else
+      title = Title.new(
+        tmdb_id: cast_entry["id"],
+        title: cast_entry["title"],
+        character: cast_entry["character"],
+        release_date: cast_entry["release_date"],
+        media_type: cast_entry["media_type"],
+        popularity: cast_entry["popularity"],
+        synced_at: DateTime.now
+      )
+      if title.valid?
+        title.save!
+        import_summary.inserted()
+      else
+        import_summary.errored()
+      end
+    end
+  end
+end
