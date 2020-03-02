4bf52534e6b55829d727b4e894ba4f495561e77b
Julian Doherty (madlep) <madlep@madlep.com>
2020-03-02 22:59:52 +1100

    fix namespace for V05::ImportSummary

--- lib/v05/import_summary.rb
+++ lib/v05/import_summary.rb
@@ -1,17 +1,19 @@
-V05::ImportSummary = Value.new(
-  :inserted_count,
-  :updated_count,
-  :error_count
-) do
-  def inserted()
-    self.with(inserted_count: self.inserted_count + 1)
-  end
+module V05
+  ImportSummary = Value.new(
+    :inserted_count,
+    :updated_count,
+    :error_count
+  ) do
+    def inserted()
+      self.with(inserted_count: self.inserted_count + 1)
+    end
 
-  def updated()
-    self.with(updated_count: self.updated_count + 1)
-  end
+    def updated()
+      self.with(updated_count: self.updated_count + 1)
+    end
 
-  def errored()
-    self.with(error_count: self.error_count + 1)
+    def errored()
+      self.with(error_count: self.error_count + 1)
+    end
   end
 end
